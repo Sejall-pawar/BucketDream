474bc3a8bf525aef04758af174cb8b0aa7d02a28
Sejall-pawar <sejalpawar463@gmail.com>
2024-11-30 19:13:18 +0530

    completed the show wish page

--- src/views/Show/Show.js
+++ src/views/Show/Show.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Show.css'
+import HomeButton from "./../../components/Homebutton/Homebutton"
+import WishCard from '../../components/WishCard/WishCard';
 
 function Show() {
+
+  const [wishes, setwish] = useState([]);
+
+  useEffect(()=>{
+    const savedwish = JSON.parse(localStorage.getItem("wishes")) ||
+    [];
+
+    setwish(savedwish)
+  }, []);
+   
+
+
+
   return (
-    <div>Show</div>
+    <div>
+      <h1 className='show'>Show Wish 📖</h1>
+      {
+        wishes.map((wish, index)=>{
+          const {title, description, targetDate, category} = wish;
+
+          return (
+           <WishCard
+           index={index}
+           title={title}
+           description={description}
+           targetdate={targetDate}
+           category={category}
+           />
+          )
+        })
+      }
+      
+
+      <HomeButton/>
+    </div>
   )
 }
 
